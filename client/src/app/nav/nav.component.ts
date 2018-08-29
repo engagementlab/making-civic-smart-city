515c4b1f11582c74ae186487f05396d4fcc6268d
Johnny Richardson <johnny.c.rich@gmail.com>
2018-08-29 16:37:24 -0400

    Nav updates, fixes for links on home.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,10 @@ import { Router, NavigationStart } from '@angular/router';
 import { TweenMax, TimelineLite } from "gsap";
 
 import { filter } from 'rxjs/operators';
+
+import { DataService } from '../data.service';
+import { PlaysService } from '../plays.service';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,34 +14,44 @@ import { filter } from 'rxjs/operators';
 })
 export class NavComponent implements OnInit {
 
-	public cities: string[] = [
-		"Boston",
-		"New York",
-		"Los Angeles",
-		"Chicago",
-		"Philadelphia",
-		"Miami"
-	];
+	public plays: any[];
+
 	tl: TimelineLite; 
 
-  constructor(private router: Router) {
+  constructor(private _dataSvc: DataService, private _playsSvc: PlaysService, private router: Router) {
 
+    // Hide nav when nav occurs
     router.events.pipe(filter(e => e instanceof NavigationStart)).subscribe(e => {
       this.tl.reverse();
     });
+
+    // Get plays if needed
+    if(!this._playsSvc.plays) { 
+      this._dataSvc.getFilteredDataForUrl('play', 'name%20blurb%20key').subscribe(response => {
+
+        this._playsSvc.plays = response;
+        this.plays = response;
+      
+      });
+    }
 
   }
 
   ngOnInit() {
 
+
   	this.tl = new TimelineLite({paused:true});
+
   	let tl = this.tl;
   	let open = document.getElementById('open');
   	let close = document.getElementById('close');
   	let navEl = document.getElementById('menu');
-  	tl.fromTo(open, .2, {autoAlpha:1}, {autoAlpha:0, rotateY:'90deg'});
-  	tl.fromTo(close, .2, {autoAlpha:0}, {autoAlpha:1, display:'block'}, '+=0.2');
-  	tl.fromTo(navEl, .6, {autoAlpha:0}, {height:'100%', autoAlpha:1, ease:Back.easeOut}, '+=0.1');
+
+    TweenLite.set(open, {transformStyle:'preserve-3d'});
+    TweenLite.set(close, {transformStyle:'preserve-3d'});
+  	tl.fromTo(open, .2, {autoAlpha:1}, {autoAlpha:0, rotationY:90});
+  	tl.fromTo(close, .2, {autoAlpha:0, rotationY:90}, {autoAlpha:1, rotationY:0, display:'block'}, '+=0.1');
+  	tl.fromTo(navEl, .6, {autoAlpha:0}, {top:0, autoAlpha:1, ease:Quad.easeOut}, '+=0.1');
 
   }
 
@@ -49,7 +63,9 @@ export class NavComponent implements OnInit {
   }
 
   closeNav() {
+
   	this.tl.reverse();
+
   }
 
 }
