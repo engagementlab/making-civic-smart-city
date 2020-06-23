855b536d8234f9a5512e4348512d733f6e5c2580
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-23 12:37:33 -0400

    refactor(client): route /about redirects to /betablocks.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,56 +1,60 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http'; 
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 // Services
-import { RouterStateService } from './routerstate.service';
-import { DataService } from './data.service';
-import { PlaysService } from './plays.service';
+import { RouterStateService } from "./routerstate.service";
+import { DataService } from "./data.service";
+import { PlaysService } from "./plays.service";
 
 // NPM
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
-import { Cloudinary as CloudinaryCore } from 'cloudinary-core';
-import { CloudinaryConfiguration, CloudinaryModule } from '@cloudinary/angular-5.x';
-import cloudinaryConfiguration from './cdn.config';
+import { ScrollToModule } from "@nicky-lenaers/ngx-scroll-to";
+import { Cloudinary as CloudinaryCore } from "cloudinary-core";
+import {
+  CloudinaryConfiguration,
+  CloudinaryModule,
+} from "@cloudinary/angular-5.x";
+import cloudinaryConfiguration from "./cdn.config";
 export const cloudinary = {
-  Cloudinary: CloudinaryCore
+  Cloudinary: CloudinaryCore,
 };
 export const config: CloudinaryConfiguration = cloudinaryConfiguration;
 
 // App components
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CdnImageComponent } from './cdn-image/cdn-image.component';
-import { ButtonComponent } from './button/button.component';
-import { NavComponent } from './nav/nav.component';
-import { CitiesComponent } from './cities/cities.component';
-import { CityComponent } from './cities/city/city.component';
-import { PlaybookComponent } from './playbook/playbook.component';
-import { LearnMoreBtnComponent } from './learn-more-btn/learn-more-btn.component';
-import { WorkshopComponent } from './workshop/workshop.component';
-import { PlayComponent } from './playbook/play/play.component';
-import { AgendaComponent } from './agenda/agenda.component';
-import { AgendastepComponent } from './agenda/agendastep/agendastep.component';
-import { PeopleComponent } from './about/people/people.component';
-import { SplitPipe } from './split.pipe';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { CdnImageComponent } from "./cdn-image/cdn-image.component";
+import { ButtonComponent } from "./button/button.component";
+import { NavComponent } from "./nav/nav.component";
+import { CitiesComponent } from "./cities/cities.component";
+import { CityComponent } from "./cities/city/city.component";
+import { PlaybookComponent } from "./playbook/playbook.component";
+import { LearnMoreBtnComponent } from "./learn-more-btn/learn-more-btn.component";
+import { WorkshopComponent } from "./workshop/workshop.component";
+import { PlayComponent } from "./playbook/play/play.component";
+import { AgendaComponent } from "./agenda/agenda.component";
+import { AgendastepComponent } from "./agenda/agendastep/agendastep.component";
+import { PeopleComponent } from "./about/people/people.component";
+import { SplitPipe } from "./split.pipe";
 
 // App routes
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },  
-  { path: 'about/who', component: PeopleComponent },  
-  
-  { path: 'agenda/:step', component: AgendaComponent },
-  { path: 'agenda', redirectTo: 'agenda/introduction', pathMatch: 'full' },
-  
-  { path: 'cities', component: CitiesComponent },
-  { path: 'playbook', component: PlaybookComponent },
-  { path: 'workshop', component: WorkshopComponent },
-  { path: 'playbook/:key', component: PlayComponent }
+  { path: "", component: HomeComponent },
+  { path: "about", redirectTo: "betablocks" },
+  { path: "about/who", component: PeopleComponent },
+  { path: "betablocks", component: AboutComponent },
+
+  { path: "agenda/:step", component: AgendaComponent },
+  { path: "agenda", redirectTo: "agenda/introduction", pathMatch: "full" },
+
+  { path: "cities", component: CitiesComponent },
+  { path: "playbook", component: PlaybookComponent },
+  { path: "workshop", component: WorkshopComponent },
+  { path: "playbook/:key", component: PlayComponent },
 ];
 
 @NgModule({
@@ -70,7 +74,7 @@ export const routes: Routes = [
     AgendaComponent,
     AgendastepComponent,
     PeopleComponent,
-    SplitPipe
+    SplitPipe,
   ],
   imports: [
     BrowserModule,
@@ -78,15 +82,10 @@ export const routes: Routes = [
     CloudinaryModule.forRoot(cloudinary, config),
     HttpClientModule,
     RouterModule.forRoot(routes),
-    ScrollToModule.forRoot()
+    ScrollToModule.forRoot(),
   ],
   exports: [RouterModule],
-  providers: [
-    RouterStateService,
-    DataService,
-    PlaysService
-  ],
-  bootstrap: [AppComponent]
+  providers: [RouterStateService, DataService, PlaysService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-  
+export class AppModule {}
